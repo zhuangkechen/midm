869f44a4d319716d86712dd83c0d43cf26324151
zhuangkechen <zhuangkechen@gmail.com>
2015-04-13 23:35:27 +0800

    fixed

--- net_2/run_test.sh
+++ net_2/run_test.sh
@@ -1,11 +1,11 @@
 # get tweets
-#pyspark ~/kechen/mb_codes/spark_analysis/net_2/divide_test.py
-#pyspark ~/kechen/mb_codes/spark_analysis/net_2/divide_training.py
+pyspark ~/kechen/mb_codes/spark_analysis/net_2/divide_test.py
+pyspark ~/kechen/mb_codes/spark_analysis/net_2/divide_training.py
 # the model
-#pyspark ~/kechen/mb_codes/spark_analysis/net_2/real_DTM.py
+pyspark ~/kechen/mb_codes/spark_analysis/net_2/real_DTM.py
 
 # get tweets
-pyspark ~/kechen/mb_codes/spark_analysis/net_sub/divide_test.py
-pyspark ~/kechen/mb_codes/spark_analysis/net_sub/divide_training.py
+#pyspark ~/kechen/mb_codes/spark_analysis/net_sub/divide_test.py
+#pyspark ~/kechen/mb_codes/spark_analysis/net_sub/divide_training.py
 # the model
-pyspark ~/kechen/mb_codes/spark_analysis/net_sub/real_DTM.py
+#pyspark ~/kechen/mb_codes/spark_analysis/net_sub/real_DTM.py
---
+++ net_sub/run_sub.sh
@@ -0,0 +1,8 @@
+# get tweets
+pyspark ~/kechen/mb_codes/spark_analysis/net_sub/sub_binladen_tweets.py
+pyspark ~/kechen/mb_codes/spark_analysis/net_sub/sub_binladen_pre_tweets.py
+pyspark ~/kechen/mb_codes/spark_analysis/net_sub/sub_binladen_after_tweets.py
+
+pyspark ~/kechen/mb_codes/spark_analysis/net_sub/sub_gaddafi_tweets.py
+pyspark ~/kechen/mb_codes/spark_analysis/net_sub/sub_gaddafi_pre_tweets.py
+pyspark ~/kechen/mb_codes/spark_analysis/net_sub/sub_gaddafi_after_tweets.py
