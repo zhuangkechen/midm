4961c6e254282bd7386aadb0ab51d5e70862b82b
zhuangkechen <zhuangkechen@gmail.com>
2015-04-15 11:24:38 +0800

    add plots

---
+++ plots/plot_feature_corr.py
@@ -0,0 +1,19 @@
+"""
+This plot codes is for ploting the features's correlations.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Example data
+people = ('Tom', 'Dick', 'Harry', 'Slim', 'Jim')
+y_pos = np.arange(len(people))
+performance = 3 + 10 * np.random.rand(len(people))
+error = np.random.rand(len(people))
+
+plt.barh(y_pos, performance, xerr=error, align='center', alpha=0.4)
+plt.yticks(y_pos, people)
+plt.xlabel('Performance')
+plt.title('How fast do you want to go today?')
+
+plt.show()
